fb1eadf7f553e941c4ccb7f38f6278ea9689f94b
gitusercz <czibere.a@gmail.com>
2021-11-11 15:02:52 +0100

    Initial submit of the Cuckoo-clock

--- Cuckoo.py
+++ Cuckoo.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python 
 
-# This program is supposed to react to each keypress by playing a sound signal individually. 
-# In subfolder Sounds there are multiple folders. Name of the folders are assigned to keypresses.
+# This program is to simulate a cuckoo-clock
+# In subfolder Sounds there are multiple folders. This script checks every minute if there is an associated folder
+# in Sounds. If there is a folder it plays whatever mp3 it finds there. 
 # There should be one wav or mp3 file in that folder, name does not matter as long as it does not have any spaces in the name
 
 # Program relies on mpg123 to be installed: 
@@ -12,11 +13,11 @@
 #	The -q option to mpg123 suppresses diagnostic messages. You can remove it if you'd like to see song titles.
 #	The sleep(0.1) call is necessary to avoid spawning tons of mpg123 calls from a single button press.
  
-import sys, termios, tty, os, time
+import sys, termios, tty, os, time, datetime
 from time import strftime
 from datetime import datetime
 
-print("FunnySound has started, waiting for all services to start..")
+print("Cuckoo clock has started, waiting for all services to start..")
 time.sleep(10)
 print("Ok, ready! \n")
  
@@ -34,7 +35,7 @@ def getch():
 def logfileupdater(logfileupdater_logfile_fullpath,logfileupdater_message_tobelogged):
 	if os.path.isfile(logfileupdater_logfile_fullpath) == False:
 		logfile = open(logfileupdater_logfile_fullpath,"w")
-		logfile.write("This is a logfile for FunnySound scripts. . Records all events with timestamps. \n\nTimestamp,Event\n") 
+		logfile.write("This is a logfile for Cuckoo-clock. Records all events with timestamps. \n\nTimestamp,Event\n") 
 		logfile.close()
 	else:
 		with open(logfileupdater_logfile_fullpath,"a") as log:
@@ -42,49 +43,37 @@ def logfileupdater(logfileupdater_logfile_fullpath,logfileupdater_message_tobelo
 
 def pressedcharacter_handler(pressedcharacter_handler_capturedchar):
 	currentchar = pressedcharacter_handler_capturedchar
-	print(currentchar + " pressed")
-	for root, dirs, files in os.walk(leadingpath + '/Sounds/'+currentchar+'/'):
-		for file in files:
-			if file.endswith(".mp3"):
-				finalpath = os.path.join(root, file)
-	logfileupdater(local_log_file_fullpath,currentchar + " pressed. Playing: "+ finalpath)
-	os.system('mpg123 -q '+ finalpath +' &')
-	time.sleep(button_delay)
+	print(currentchar + " is the time")
+        if os.path.isdir(leadingpath + '/Sounds/'+currentchar+'/') == True:
+	        print(currentchar + " has an associated sound to play! ")
+		for root, dirs, files in os.walk(leadingpath + '/Sounds/'+currentchar+'/'):
+			for file in files:
+				if file.endswith(".mp3"):
+					finalpath = os.path.join(root, file)
+					logfileupdater(local_log_file_fullpath,currentchar + " pressed. Playing: "+ finalpath)
+		logfileupdater(local_log_file_fullpath,currentchar + " is the time.")
+		os.system('mpg123 -q '+ finalpath +' &')
+		
+	else:
+		print(currentchar + " does not have an associated folder! ")
 	
 
 
 	
  
-button_delay = 0.2		# needed for debouncing and spawning a lot of os.system calls. 
+check_frequency = 60		# needed for debouncing and spawning a lot of os.system calls. 
 leadingpath, runningfile = os.path.split(os.path.realpath(__file__))   # splitting /home/pi/dev/run.py to /home/pi/dev  AND run.py 
 #With thios method the folder, where the script is can be querried. This makes the python script portable. 
-local_log_file_fullpath = leadingpath + '/FunnyLog.txt'
+local_log_file_fullpath = leadingpath + '/CuckooLog.txt'
 
 #logging to the logfile
 with open(local_log_file_fullpath,"a") as log:
 			log.write("\n\n"+strftime("%Y-%m-%d %H:%M:%S")+","+"Started")
  
 while True:
-    char = getch()
- 
-    if (char == "q"):
-        print("Stop!")
-        os.system('killall mpg123')
-        logfileupdater(local_log_file_fullpath,"q pressed, exiting.")
-        exit(0)
- 
-    elif (char == "w"):
-        pressedcharacter_handler("w")
-    elif (char == "e"):
-        pressedcharacter_handler("e")
-    elif (char == "r"):
-        pressedcharacter_handler("r")
-    elif (char == "t"):
-        pressedcharacter_handler("t")
-    elif (char == "z"):
-        pressedcharacter_handler("z")
-        
- 
-    elif (char == "1"):
-        print("Number 1 pressed")
-        time.sleep(button_delay)
+    now = datetime.now()
+    timeStr = now.strftime("%H%M")
+    print (timeStr)
+    pressedcharacter_handler(timeStr)
+    time.sleep(check_frequency)
+    
